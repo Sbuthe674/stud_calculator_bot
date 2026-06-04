ddd8cf794da0e738efab6216e0e5508ca2f81478
kz-proger <diasikoni@gmail.com>
2026-06-04 15:14:15 +0500

    Update __init__.py

--- calculations/__init__.py
+++ calculations/__init__.py
@@ -1,1 +1,14 @@
+from .average import calculate_average, calculate_average_with_percent
+from .forecast import calculate_exam_score
+from .gpa import calculate_gpa, convert_grade, percent_to_gpa
+from .rating import calculate_rating
 
+__all__ = [
+    "calculate_average",
+    "calculate_average_with_percent",
+    "calculate_exam_score",
+    "calculate_gpa",
+    "convert_grade",
+    "percent_to_gpa",
+    "calculate_rating",
+]
